38a6543462005e63772e2e0a840e49ba0a81a735
Biswajit Rout <b119018@iiit-bh.ac.in>
2021-02-07 12:34:44 +0530

    third edit

--- src/App.js
+++ src/App.js
@@ -2,17 +2,15 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
-const HatsPage = () => (
-  <div>
-    <h1>HATS PAGE</h1>
-  </div>
-);
+import ShopPage from './pages/shop/shop.component.jsx';
+
+
 function App() {           
   return (                     //switch- as soon as it sees a path is matching then it will not render any another page, but then because of '/' it will always render homepage so to avoid that we can also have exact it consists of boolean values by default exact is true 
     <div>                              
       <Switch>                      
           <Route exact path='/' component={HomePage} />
-          <Route path='/hats' component={HatsPage} />
+          <Route path='/shop' component={ShopPage} />
       </Switch>
     </div>
   );
