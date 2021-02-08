c56cedc2e3e7ba61bb62bea0e4494d572451d271
Biswajit Rout <b119018@iiit-bh.ac.in>
2021-02-08 22:51:32 +0530

    added header component

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import { Switch, Route } from 'react-router-dom';
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component.jsx';
+import Header from './components/header/header.component.jsx'
 
 
 function App() {           
   return (                     //switch- as soon as it sees a path is matching then it will not render any another page, but then because of '/' it will always render homepage so to avoid that we can also have exact it consists of boolean values by default exact is true 
-    <div>                              
+    <div>
+      <Header />                              
       <Switch>                      
           <Route exact path='/' component={HomePage} />
           <Route path='/shop' component={ShopPage} />
--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -3,7 +3,7 @@ import SHOP_DATA from './shop.data.js';
 import CollectionPreview from '../../components/collection-preview/collection-preview.component';
 
 class ShopPage extends React.Component {
-    constructor(props) {
+    constructor(props) {      //So, to conclude, If you want to use this.props inside constructor, you need to pass it in super, otherwise it’s okay to not pass props to super as we see that irrespective of passing it to super, this.props is available inside render function.
         super(props);
 
         this.state = {
