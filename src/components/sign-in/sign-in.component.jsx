46f1e11f079f8f07afed99d77a31ed3ae7bc604f
Biswajit Rout <b119018@iiit-bh.ac.in>
2021-03-07 23:48:27 +0530

    finished handle submit method

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 
 import FormInput from '../form-input/form-input.component';
 import CustomButton from '../custom-button/custom-button.component';
-import { signInWithGoogle } from '../../firebase/firebase.utils';
+import {auth, signInWithGoogle } from '../../firebase/firebase.utils';
 import './sign-in.styles.scss';
 
 class SignIn extends React.Component{              //we are using class component here because we need this keyword to be used which will be used to store the data
@@ -15,10 +15,17 @@ class SignIn extends React.Component{              //we are using class componen
             password: ''
         };
     }
-    handleSubmit = event => {
+    handleSubmit = async event => {
         event.preventDefault();   //it prevents an event from taking place here it is preventing the form to get submitted because we want full control over it
-        this.setState({ email: '',password: ''});
-    }
+        const { email, password } = this.state;
+
+        try {
+            await auth.signInWithEmailAndPassword(email, password);
+            this.setState({ email: '',password: ''});
+        } catch(error) {
+            console.log(error);
+        }
+    };
 
     handleChange = event => {
         const { value,name } = event.target;
