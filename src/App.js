d603118e3440c3245702a3086344d93f5cacd707
Biswajit Rout <b119018@iiit-bh.ac.in>
2021-03-09 11:11:33 +0530

    implemented userActions

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
+import { connect } from 'react-redux';
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
@@ -7,33 +8,28 @@ import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up
 import Header from './components/header/header.component';
 
 import { auth, createUserProfileDocument} from './firebase/firebase.utils';
+import { setCurrentUser } from './redux/user/user.actions';
 
 class App extends React.Component {    //in this we will have access to 
-  constructor(){
-    super();
-    this.state = {
-      currentUser: null
-    };
-  }
+  
 
   unsubscribeFromAuth = null;
 
   componentDidMount() {
+    const {setCurrentUser} = this.props;
     this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {     //we are fetching data from firestore database
       if(userAuth){
         const userRef = await createUserProfileDocument(userAuth);           //userAuth is passed ... in firebase utils it checks if snapshot exists if not then it will create one and the return userRef
 
         userRef.onSnapshot(snapShot => {
-          this.setState({
-            currentUser: {
+           setCurrentUser({
               id: snapShot.id,
               ...snapShot.data()
-            }
-          });
+           });
         });
       }
 
-     this.setState({ currentUser: userAuth});     //when the user logs out the currentuser value is set to null
+     setCurrentUser(userAuth);     //when the user logs out the currentuser value is set to null
     });
   }
   componentWillUnmount() {
@@ -54,4 +50,8 @@ class App extends React.Component {    //in this we will have access to
   }         
 }
 
-export default App;
+const mapDispatchToProps = dispatch => ({
+  setCurrentUser: user => dispatch(setCurrentUser(user))         //whatever object we are passing in dispatch tells redux that it is an action object that needs to be passed to rest of the applications
+});
+
+export default connect(null, mapDispatchToProps)(App);
