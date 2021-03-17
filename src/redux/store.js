506d8a5a32e8766563edaa91d9a8b19180a2e14f
Biswajit Rout <b119018@iiit-bh.ac.in>
2021-03-17 21:34:30 +0530

    adding cartitems count

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,6 @@
 import { createStore, applyMiddleware } from 'redux';
 import logger from 'redux-logger';
-
+//it shows everything in console which can be used by others in application
 import rootReducer from './root-reducer';
 
 const middlewares = [logger];
