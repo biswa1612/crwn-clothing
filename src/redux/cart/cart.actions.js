2873f37d378ee5b4541aaab28c5cf5d0cb0c00e1
Biswajit Rout <b119018@iiit-bh.ac.in>
2021-03-20 02:09:40 +0530

    adding and removing item

--- src/redux/cart/cart.actions.js
+++ src/redux/cart/cart.actions.js
@@ -7,3 +7,13 @@ import CartActionTypes from './cart.types';
      type: CartActionTypes.ADD_ITEM,
      payload: item
  });
+
+ export const removeItem = item => ({
+     type: CartActionTypes.REMOVE_ITEM,
+     payload: item
+ });
+
+ export const clearItemFromCart = item => ({
+     type: CartActionTypes.CLEAR_ITEM_FROM_CART,
+     payload: item
+ });
--- src/redux/cart/cart.reducer.js
+++ src/redux/cart/cart.reducer.js
@@ -1,5 +1,6 @@
+import CartItem from '../../components/cart-item/cart-item.component';
 import CartActionTypes from './cart.types';
-import { addItemToCart } from './cart.utils';
+import { addItemToCart, removeItemFromCart } from './cart.utils';
 const INITIAL_STATE = {
     hidden: true,
     cartItems: []
@@ -16,7 +17,17 @@ const cartReducer = (state = INITIAL_STATE, action) => {
             return {
                 ...state,
                 cartItems: addItemToCart(state.cartItems, action.payload)
-            }
+            };
+        case CartActionTypes.REMOVE_ITEM:
+            return {
+                ...state,
+                cartItems: removeItemFromCart(state.cartItems, action.payload)
+            };
+        case CartActionTypes.CLEAR_ITEM_FROM_CART:
+            return {
+                ...state,
+                cartItems: state.cartItems.filter(cartItem => cartItem.id != action.payload.id)   //it will keep the elements those whoose id wont match witch the one we want to remove and will return us a new array
+            };
         default:
             return state;
     }
